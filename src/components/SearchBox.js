c298a6f70263965402869a326ab288498207bb92
Jonathan <jonnym46@icloud.com>
2022-01-22 17:52:45 +0000

    robofriends react app

---
+++ src/components/SearchBox.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const SearchBox = ({ searchfield, searchChange}) => {
+    return (
+        <div className='pa2'>
+            <input 
+                className='pa3 ba b--grey bg-white br3 shadow-5'
+                type='search' 
+                placeholder='search robots'
+                onChange={searchChange}
+            />
+        </div>
+        
+    );
+}
+
+export default SearchBox;
